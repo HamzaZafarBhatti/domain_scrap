3a97853d15ec0c5fdef3d33ec271952315b3a3ca
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2024-02-09 15:23:50 +0500

    new changes

--- app/Http/Controllers/ScrapperController.php
+++ app/Http/Controllers/ScrapperController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\Country;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -9,25 +11,37 @@ class ScrapperController extends Controller
 {
     public function index()
     {
-        return view('admin.scrapper.index');
+        $countries = Country::select('id', 'name')->get();
+        $cities = City::select('id', 'name')->get();
+        return view('admin.scrapper.index', compact('cities', 'countries'));
     }
 
     public function start(Request $request)
     {
-        $data['keyword'] = $request->keyword;
+        // return $request;
+        // $data['keyword'] = $request->keyword;
+        // $data['additional_keyword'] = $request->additional_keyword;
         $archived_domain_names = array();
-        $location = ['canada','france'];
-        $keywords = ['realestatezzz, real estate','house for sale'];
+        $country = Country::find($request->country_id)->name;
+        $city = City::find($request->city_id)->name;
+        $location = [$country, $city];
+        // $keywords = ['realestatezzz, real estate', 'house for sale'];
+        $keywords = explode(',', $request->keyword);
         $domains = [];
-        foreach($location as $loc){
-            foreach($keywords as $key){
+        foreach ($location as $loc) {
+            foreach ($keywords as $key) {
                 $keyword = str_replace(' ', '', $key);
-                $domains[] = $keyword.$loc.'.com';
+                $domains[] = $keyword . $request->additional_keyword . $loc . '.com';
             }
         }
+        // return $keywords;
         $domain_year_data = [];
-        $years = ['2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014' ];
-        foreach($domains as $domain) {
+        for ($i = $request->year; $i <= now()->format('Y'); $i++) {
+            $years[] = $i;
+        }
+        // return $years;
+        // $years = ['2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014'];
+        foreach ($domains as $domain) {
             $domain_name = $domain;
             foreach ($years as $year) {
                 $url = 'https://web.archive.org/__wb/calendarcaptures/2?url=' . $domain_name . '&date=' . $year . '&groupby=day';
@@ -42,15 +56,18 @@ class ScrapperController extends Controller
             if (!empty($domain_year_data)) {
                 continue;
             }
-            $client = new \GuzzleHttp\Client();
-            $response = $client->request('GET', 'https://domainr.p.rapidapi.com/v2/status?mashape-key=d03abf08787645d4a17386782f11b0b7&domain=' . $domain_name, [
-                'headers' => [
-                    'X-RapidAPI-Host' => 'domainr.p.rapidapi.com',
-                    'X-RapidAPI-Key' => 'ee945fba55msh43c04ba37ae8d39p1e79d0jsn487ddd1f7dad',
-                ],
-            ]);
+            $response = Http::withHeaders([
+                'X-RapidAPI-Host' => 'domainr.p.rapidapi.com',
+                'X-RapidAPI-Key' => 'ee945fba55msh43c04ba37ae8d39p1e79d0jsn487ddd1f7dad',
+            ])->get('https://domainr.p.rapidapi.com/v2/status?mashape-key=d03abf08787645d4a17386782f11b0b7&domain=' . $domain_name);
 
-            return $response->getBody();
+            if ($response->status() == 200) {
+                $data = $response->json();
+                if (str_contains($data['status'][0]['status'], 'inactive')) {
+                    $archived_domain_names[] = $data['status'][0]['domain'];
+                }
+            }
         }
+        return back()->with('domains', $archived_domain_names);
     }
 }
--- resources/views/admin/scrapper/index.blade.php
+++ resources/views/admin/scrapper/index.blade.php
@@ -21,12 +21,54 @@
                                     <input type="text" class="form-control" name="keyword" id="keyword" required>
                                 </div>
                             </div>
-                            {{-- <div class="col-6">
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label>Additional Keyword</label>
+                                    <select name="additional_keyword" class="form-control">
+                                        <option value="">Select</option>
+                                        <option value="in">in</option>
+                                        <option value="at">at</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label>Year</label>
+                                    <select name="year" class="form-control">
+                                        <option value="2014">2014</option>
+                                        <option value="2015">2015</option>
+                                        <option value="2016">2016</option>
+                                        <option value="2017">2017</option>
+                                        <option value="2018">2018</option>
+                                        <option value="2019">2019</option>
+                                        <option value="2020">2020</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2024">2024</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-6">
                                 <div class="form-group">
                                     <label>Country</label>
-                                    <input type="text" class="form-control" name="keyword" id="keyword" required>
+                                    <select name="country_id" class="form-control" required>
+                                        @foreach ($countries as $item)
+                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                            </div> --}}
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label>Country</label>
+                                    <select name="city_id" class="form-control" required>
+                                        @foreach ($cities as $item)
+                                            <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                         <div>
                             <button class="btn btn-primary" type="submit">Start</button>
@@ -35,5 +77,34 @@
                 </div>
             </div>
         </div>
+        @if (session()->has('domains'))
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Available Domains</h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse (session('domains') as $item)
+                                    <tr>
+                                        <td>{{ $item }}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td>No Available Domains!</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
 @endsection
